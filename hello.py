125ad46d322ad1910d8c0032c32959a52a0693d1
LR <acecardii@qq.com>
2017-08-12 20:04:05 +0800

    Capter 4 finished.

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, url_for, redirect, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -19,28 +19,41 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
 	name = StringField('What is your name?', validators=[data_required()])
-	accept_tos = BooleanField('I accept the TOS', validators=[data_required()])
+	accept_tos = BooleanField('I accept the TOS', validators=[data_required()], default='checked')
 	submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():#首页用来测试新技术
-	name = None
+def index():  # 首页用来测试新技术
 	form = NameForm()
+	# FUNCTION NameForm
 	if form.validate_on_submit() and form.accept_tos:
-		name = form.name.data
+		# FUNCTION flash
+		if form.name.data != session.get('name') and session.get('name'):
+			flash('It seems you have changed your name!')
+		# Session remenber name
+		session['name'] = form.name.data
 		form.name.data = ''
+		# redirect to avoid pop_up_window when refresh this page
+		return redirect(url_for('index'), 302)
+	# FUNCTION ipaddress and ipinfo
 	ipaddr = request.remote_addr
 	ipinfo = check_ip_location(ipaddr)
-	return render_template('index.html', form=form, name=name, ipaddr=ipaddr, ipinfo=ipinfo,
+
+	return render_template('index.html', form=form, name=session.get('name'), ipaddr=ipaddr, ipinfo=ipinfo,
 	                       current_time=datetime.datetime.utcnow())
 
 
-@app.route('/mainpage')
-def mainpage():#这个用来做积累页面
+@app.route('/mainpage', methods=['GET', 'POST'])
+def mainpage():  # 这个用来做积累页面
+	form = NameForm()
+	if form.validate_on_submit():
+		session['name'] = form.name.data
+		return redirect(url_for('mainpage'), 302)
 	ipaddr = request.remote_addr
 	ipinfo = check_ip_location(ipaddr)
-	return render_template('mainpage.html', ipaddr=ipaddr, ipinfo=ipinfo, current_time=datetime.datetime.utcnow())
+	return render_template('mainpage.html', form=form, name=session.get('name'), ipaddr=ipaddr, ipinfo=ipinfo,
+	                       current_time=datetime.datetime.utcnow())
 
 
 @app.errorhandler(404)
