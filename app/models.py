2101f009a99d39af87b41984b7031e99708c2c76
LR <q209209a@163.com>
2017-08-27 23:28:24 +0800

    add password_hash function to Model.User .

--- app/models.py
+++ app/models.py
@@ -1,30 +1,43 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Role(db.Model):
-    __tablename__='roles'
+    __tablename__ = 'roles'
 
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(64),unique=True)
-    user=db.relationship('User',backref='role',lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    user = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
-        return '<Role %r>'%self.name
+        return '<Role %r>' % self.name
 
 
 class User(db.Model):
-    __tablename__='users'
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(64),unique=True,index=True)
-    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readabel attribute.')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Mylog(db.Model):
-    __tablename__='mylog'
-    id=db.Column(db.Integer,primary_key=True)
-    time=db.Column(db.DateTime)
-    ipaddr=db.Column(db.String(64))
-    ipinfo=db.Column(db.String(64))
+    __tablename__ = 'mylog'
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime)
+    ipaddr = db.Column(db.String(64))
+    ipinfo = db.Column(db.String(64))
 
     def __repr__(self):
-        return '<User %r>'%self.username
+        return '<User %r>' % self.username
