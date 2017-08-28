92524418dd33be95125dca5545e74fd2798e5727
LR <q209209a@163.com>
2017-08-28 20:07:58 +0800

    fix bug: LoginForm

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,9 @@
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import DataRequired,Length,Email
+from flask_wtf import FlaskForm
 
 
-class LoginForm():
+class LoginForm(FlaskForm):
     email=StringField('Email',validators=[DataRequired(),Length(1,64),Email()])
     password=PasswordField('Password',validators=[DataRequired()])
     remember_me=BooleanField('Keep me logged in')
