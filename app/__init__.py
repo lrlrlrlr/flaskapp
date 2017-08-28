ba5b3be946e562419543fdf87e1c5c452a23d90e
LR <q209209a@163.com>
2017-08-28 19:16:54 +0800

    add login_manager.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import config
 
@@ -12,7 +13,9 @@ manager=Manager()
 bootstrap=Bootstrap()
 moment=Moment()
 mail=Mail()
-
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
 def create_app(config_name):
     app=Flask(__name__)
@@ -23,6 +26,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     #附加路由和自定义的错误页面
     from .main import main as main_blueprint
