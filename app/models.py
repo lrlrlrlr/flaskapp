a4cf94f2bf515d50a92eeec44e4794940f6c6fae
LR <q209209a@163.com>
2017-08-30 22:09:28 +0800

    Capter 8.6.1 , add comfirm generate_confirm_token and confirm function.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 
@@ -17,23 +19,47 @@ class Role(db.Model):
 
 
 class User(UserMixin,db.Model):
+    #todo 下面db.Interger如果改成db.INTERGER还是一样的吗?
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email=db.Column(db.String(64),unique=True,index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    comfirmed=db.Column(db.Boolean,default=False)
 
     @property
     def password(self):
+        '''拒绝访问明文密码'''
         raise AttributeError('password is not a readabel attribute.')
 
     @password.setter
     def password(self, password):
+        '''生成哈希密码'''
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
+        '''验证哈希密码'''
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self,expiration=3600):
+        '''生成确认token'''
+        s=Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({
+                           'confirm':self.id})
+
+    def confirm(self,token):
+        '''确认'''
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data=s.loads(token)
+        except:
+            return False
+        if data.get('confirm')!=self.id:
+            return False
+        self.comfirmed=True
+        db.session.add(self)
+        return True
 
 
 class Mylog(db.Model):
