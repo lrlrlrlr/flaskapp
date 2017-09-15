f8df8b2cf454adbfd807ab2e3ea07825428891cd
LR <q209209a@163.com>
2017-09-15 19:45:05 +0800

    Add validate function to ShorturlquerierForm.

--- app/main/forms.py
+++ app/main/forms.py
@@ -29,12 +29,25 @@ class UVweaverForm(FlaskForm):
             raise ValidationError('范围1~20000')
 
 
-class ShorturlcheckForm(FlaskForm):
+class ShorturlquerierForm(FlaskForm):
     #批量查询短链
     short_url_list_raw=StringField('短链,多个用逗号分隔（如rUvIrmu,EjuU3q6）',validators=[data_required()])
-    start_date=StringField('输入开始日期(如20170101)',validators=[data_required()])
-    end_date=StringField('输入开始日期(如20170101)',validators=[data_required()])
+    start_date=StringField('输入开始日期(如20170901)',validators=[data_required(),Length(8,8)])
+    end_date=StringField('输入结束日期(如20170901)',validators=[data_required(),Length(8,8)])
     submit=SubmitField('Check')
+
+    def validate_short_url_list_raw(self,field):
+        '''检查短链的格式:空格,长度'''
+        if ' ' in field.data:
+            raise ValidationError('短链里面有空格,请仔细检查!')
+        for short_url in field.data.split(','):
+            if type(short_url)==str and len(short_url)!=7:
+                raise ValidationError('短链:{}长度不对!长度只能为7!'.format(short_url))
+
+    def validate_end_date(self,field):
+        '''校验日期:结束日期必须在开始日期之后'''
+        if int(field.data)<int(self.start_date.data):
+            raise ValidationError('结束日期必须在开始日期之后!')
 
 
 class EditProfileForm(FlaskForm):
