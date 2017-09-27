5ad6f4d6e42184d7157b22fc74e30cec77ea7dfb
LR <q209209a@163.com>
2017-09-27 16:50:48 +0800

    Capter 11.4.1, initialized flask-pagedown.

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_moment import Moment
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_pagedown import PageDown
 
 from config import config
 
@@ -16,6 +17,7 @@ mail=Mail()
 login_manager=LoginManager()
 login_manager.session_protection='strong'
 login_manager.login_view='auth.login'
+pagedown = PageDown()
 
 def create_app(config_name):
     app=Flask(__name__)
@@ -27,6 +29,8 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
+
 
     #附加路由和自定义的错误页面
     from .main import main as main_blueprint
